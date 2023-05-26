c4c50813649c3181321285d25e1327bd7a443f09
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2023-05-26 11:29:17 +0100

    added copilot

--- lua/jazzkid/remap.lua
+++ lua/jazzkid/remap.lua
@@ -36,10 +36,11 @@ vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
 end)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz") 
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz") 
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") 
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz") 
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
